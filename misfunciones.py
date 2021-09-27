114ff5e8168ca18f92a1bf5f2b99e98ddf5e8078
Auca <forosypaginas@gmail.com>
2021-09-27 14:41:57 -0300

    added get_daily and modified .gitignore

--- misfunciones.py
+++ misfunciones.py
@@ -11,11 +11,6 @@ def token():
     tokenlist = ['2RG2NEF3IPXMIPX3', 'ZOW97SMSE5U3FPYU']
     return tokenlist[1]
 
-"""
-# definición de variables
-symbol = 'AAPL'
-"""
-# concatenar el link
 def get_intraday(symbol, interval='15min'):
     """Get intraday values from Alphavantage
 
@@ -25,6 +20,7 @@ def get_intraday(symbol, interval='15min'):
     """
     function = 'TIME_SERIES_INTRADAY' 
 
+# concatenar el link
     urlBase = 'https://www.alphavantage.co/query'
     url = urlBase+ '?function=' + function
     url += '&symbol=' + str(symbol)
@@ -45,5 +41,31 @@ def get_intraday(symbol, interval='15min'):
     dataDF = pd.DataFrame.from_dict(data, orient='index')
     dataDF
 
+def get_daily(symbol, output='compact'):
+    """Get daily values from Alphavantage
+
+    Args:
+        symbol ([type]): [description]
+        output (compact or full, optional): [compact defaults into 100 values, full displays into 20+ years of historical data].
+    """
+    function = 'TIME_SERIES_DAILY' 
+    urlBase = 'https://www.alphavantage.co/query' 
+    url = urlBase+ '?function=' + function
+    url += '&symbol=' + str(symbol)
+    url += '&outputsize=compact'
+    url += '&apikey=' + token()
+    
+    # print(url)
+    # print('https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=demo')
+    # acá hago el llamado/request
+
+    r = requests.get(url)
+    # r = requests.get(url)
+    print(r.json())
+
+    data = r.json()['Time Series (Daily)']
+    dataDF = pd.DataFrame.from_dict(data, orient='index')
+    dataDF
+
 if __name__ == "__main__":
-    get_intraday('AAPL')
+    get_daily('AAPL')
